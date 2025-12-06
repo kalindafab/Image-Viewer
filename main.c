296119a730a4c3536eb48be5dca7b56fd9881ed3
kalindafab <kfablionel@gmail.com>
2025-12-06 20:55:58 +0200

    image viewer in C

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<SDL2/SDL.h>
+
+int main() {
+
+    printf("Hello, ma nigga!\n" );
+   SDL_Window *pwindow SDL_CreateWindow("Image viewer",SLD_WINDOWSPOS_CENTERED ,SLD_WINDOWSPOS_CENTERED, 900,
+   600, 0);
+    
+}
